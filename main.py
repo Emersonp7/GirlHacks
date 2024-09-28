9706030c2b1fec85676dc972a819084f7bde7482
Emersonp7 <emersonp935@gmail.com>
2024-09-28 13:53:19 -0400

    client info added

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import spotipy as sp
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 clientId = "4c9e4fb01bd345989368485e373fcc19"
 clientSecret = "f56247ad580b433681d4edadccef32f6"
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=clientId, client_secret=clientSecret))
 
