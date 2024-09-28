4b1afb4c6cb92b5ea19aaf9879ef4aff40fe526c
Emersonp7 <emersonp935@gmail.com>
2024-09-28 14:50:50 -0400

    ...

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ clientSecret = "f56247ad580b433681d4edadccef32f6"
 
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=clientId, client_secret=clientSecret))
 
+temp = "6apkynw8DBUza2600Wm70t"
+print(sp.album(temp))
+
+https://open.spotify.com/user/mgyr4e4lvgz59562k8h02lh00?si=a12f944902da46ec
