1c3b77f1cc5f02ccb2d28d931a3c0b059292a6da
msv28 <sleepyheroo@gmail.com>
2024-09-28 15:36:17 -0400

    Checkout and Main website

--- Website/main.js
+++ Website/main.js
@@ -1,7 +1,7 @@
 document.getElementById('myBtn').addEventListener('click', function() {
-    const clientId = '4c9e4fb01bd345989368485e373fcc19'; // Replace with your actual Client ID
-    const redirectUri = 'localhost:3000/checkout.html'; // Replace with your actual Redirect URI
-    const scopes = 'user-read-private user-read-email'; // Add any other scopes you need
+    const clientId = '4c9e4fb01bd345989368485e373fcc19';
+    const redirectUri = 'localhost:3000/callback';
+    const scopes = 'user-read-private user-read-email';
 
     const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
 
