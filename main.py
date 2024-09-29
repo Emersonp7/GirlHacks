aafc669d07c0d949d07bae5a8a8cd5b4578b65cf
Emersonp7 <emersonp935@gmail.com>
2024-09-28 23:43:38 -0400

    all files so far together

--- main.py
+++ main.py
@@ -10,9 +10,36 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=clientId,
                                                 redirect_uri=redirectUri, 
                                                 scope="user-top-read"))
 
-tracks = sp.current_user_top_tracks(limit=5)
+artists = sp.current_user_top_artists(limit=50)
 
 # Tracks["items"][index]["name"]
 # Index is index of songs in the list
 
-print(len(tracks["items"]))
+
+class Spotify:
+    def __init__(self, clientID, clientSecret, redirectUrl, scope) -> None:
+        self.clientID = clientID
+        self.clientSecret = clientSecret
+        self.redirectUrl = redirectUrl
+        self.scope = scope
+        self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=clientID, 
+                                               client_secret=clientSecret, 
+                                                redirect_uri=redirectUrl, 
+                                                scope=scope))
+    def getTopTracks(self, limit) -> list[str]:
+        tracks = self.sp.current_user_top_tracks(limit=limit)
+        tracklist = []
+        for i in range(len(tracks["items"])):
+            tracklist.append(tracks["items"][i]["name"])
+        return tracklist
+    def getTopArtists(self, limit) -> list[str]:
+        artists = self.sp.current_user_top_artists(limit=limit)
+        artistlist = []
+        for i in range(len(artists["items"])):
+            artistlist.append(artists["items"][i]["name"])
+        return artistlist
+
+
+# spotify = Spotify(clientId, clientSecret, redirectUri, "user-top-read")
+# print(spotify.getTopTracks(5))
+# print(spotify.getTopArtists(5))
