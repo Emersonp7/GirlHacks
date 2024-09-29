f981def49a56bd3a111dfdbe3f9eb1601bc3c1f0
msv28 <sleepyheroo@gmail.com>
2024-09-29 09:19:39 -0400

    Final.html and checkout changes

--- Website/main.js
+++ Website/main.js
@@ -1,9 +1,15 @@
 document.getElementById('myBtn').addEventListener('click', function() {
-    const clientId = '4c9e4fb01bd345989368485e373fcc19';
-    const redirectUri = 'localhost:3000/callback';
+    const clientId = '4c9e4fb01bd345989368485e373fcc19'; 
+    const redirectUri = 'https://localhost:3000/callback';
     const scopes = 'user-read-private user-read-email';
 
     const authUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${encodeURIComponent(scopes)}&redirect_uri=${encodeURIComponent(redirectUri)}`;
 
     window.location.href = authUrl;
 });
+window.onload = function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('code')) {
+        window.location.href = 'checkout.html';
+    }
+};
