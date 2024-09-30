86fb5401a9e976376ed825a38a078d3d4414a6bb
Emersonp7 <emersonp935@gmail.com>
2024-09-29 22:07:23 -0400

    ...

--- main.py
+++ main.py
@@ -3,28 +3,6 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 app = Flask(__name__)
-
-clientId = "4c9e4fb01bd345989368485e373fcc19"
-clientSecret = "f56247ad580b433681d4edadccef32f6"
-redirectUri = "https://localhost:3000/callback"
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=clientId, 
-                                               client_secret=clientSecret, 
-                                                redirect_uri=redirectUri, 
-                                                scope="user-top-read"))
-
-# i = 0
-# print(artists["items"][i]["name"], artists["items"][i]['genres'])
-
-# Tracks["items"][index]["name"]
-# Index is index of songs in the list
-
-songs = sp.current_user_top_tracks(limit=50, time_range="long_term")
-for i in range(len(songs["items"])):
-    print(songs["items"][i]["name"])
-
-
-
 class Spotify:
     def __init__(self, clientID, clientSecret, redirectUrl, scope) -> None:
         self.clientID = clientID
@@ -49,8 +27,6 @@ class Spotify:
             artistlist.append(artists["items"][i]["name"])
         return artistlist
 
-<<<<<<< HEAD
-=======
 @app.route('/top-tracks')
 def top_tracks():
     spotify = Spotify(clientID='4c9e4fb01bd345989368485e373fcc19', clientSecret='f56247ad580b433681d4edadccef32f6', redirectUrl='https://localhost:3000/callback', scope='user-top-read')
@@ -60,7 +36,6 @@ def top_tracks():
 if __name__ == '__main__':
     app.run(debug=True)
 
->>>>>>> f6256b91d556f6a20b9c56d779549a3e376e3c56
 # spotify = Spotify(clientId, clientSecret, redirectUri, "user-top-read")
 # print(spotify.getTopTracks(5))
 # print(spotify.getTopArtists(5))
